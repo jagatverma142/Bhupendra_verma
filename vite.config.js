baad7e8094baffd0e1106b04af1839ea5b971dba
anishav473 <you@example.com>
2026-01-15 22:45:27 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        base: "Bhupendra_verma",
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  base: "/Bhupendra_verma/",   // yaha apni GitHub repo ka naam likho
+  plugins: [react()],
 })
