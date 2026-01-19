6f0d7deb6e5943f1fd8037220a8b64cdbc915e9e
anishav473 <you@example.com>
2026-01-20 00:02:42 +0530

    Added resume pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar'; // Ensure Navbar.jsx file exists here
 import Bhupendra_verma from './Pages/Bhupendra_verma';
 import About from './Pages/About';
