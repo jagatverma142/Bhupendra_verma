d1cdc691c2f796d749892c12b3235e2ba9013640
anishav473 <you@example.com>
2026-01-20 01:01:31 +0530

    Added resume pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar'; // Ensure Navbar.jsx file exists here
-import Bhupendra_verma from './Pages/Bhupendra_verma';
+import Bhupendra_verma from './Pages/Home';
 import About from './Pages/About';
 import Projects from './Pages/Projects';
 import './index.css';
@@ -17,7 +17,7 @@ function App() {
       <Navbar /> 
       
       <Routes>
-        <Route path="/Bhupendra_verma" element={<Bhupendra_verma />} />
+        <Route path="/home" element={<Bhupendra_verma />} />
         <Route path="/about" element={<About />} />
         <Route path="/Projects" element={<Projects />} />
         <Route path="/Services" element={<Services />} />
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "https://jagatverma142.github.io/Bhupendra_verma/#/Bhupendra_verma",   // yaha apni GitHub repo ka naam likho
+  base: "/Bhupendra_verma",   // yaha apni GitHub repo ka naam likho
   plugins: [react()],
 })
