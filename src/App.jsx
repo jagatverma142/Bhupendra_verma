c61dacc5c358793aad5faafc8e117a532179a742
anishav473 <you@example.com>
2026-01-20 00:10:25 +0530

    Added resume pdf

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <Navbar /> 
       
       <Routes>
-        <Route path="/Bhupendra_verma/" element={<Bhupendra_verma />} />
+        <Route path="/Home" element={<Bhupendra_verma />} />
         <Route path="/about" element={<About />} />
         <Route path="/Projects" element={<Projects />} />
         <Route path="/Services" element={<Services />} />
