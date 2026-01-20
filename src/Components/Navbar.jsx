a6288ef9a6d1a2bbc1eb0b27ec9a834a80180cc8
anishav473 <anishav473@gmail.com>
2026-01-20 10:01:53 +0530

    ok

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   // Dynamic Navigation Items
   const navItems = [
-    { name: 'Home', path: '/home' },
+    { name: 'Home', path: '/' },
     { name: 'About', path: '/about' },
     { name: 'Projects', path: '/projects' },
     { name: 'Services', path: '/services' },
