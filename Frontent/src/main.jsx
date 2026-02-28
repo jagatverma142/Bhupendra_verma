1e1d8900a97e93e8da1ca4a343b81fd6d8493891
anishav473 <you@example.com>
2026-02-28 15:03:47 +0530

    ok

--- Frontent/src/main.jsx
+++ Frontent/src/main.jsx
@@ -7,6 +7,7 @@ import App from './App.jsx'
 
 // ✅ Import Vercel Analytics
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
